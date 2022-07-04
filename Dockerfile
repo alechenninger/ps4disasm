8d29c199d11a5ae626983e06c8c87628c4b314fb
Alec Henninger <alechenninger@gmail.com>
2022-07-04 08:51:28 -0400

    Put p2bin binary outside tmp

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN cd asl-releases && cp Makefile.def.tmpl Makefile.def && make -i install
 # Compile custom p2bin
 # Not sure if the asl p2bin can be made to work but :shrug:
 ADD AS/ps4p2bin.c /tmp/
-RUN gcc -O3 -w -o /tmp/p2bin /tmp/ps4p2bin.c
+RUN gcc -O3 -w -o /usr/bin/ps4p2bin /tmp/ps4p2bin.c
--- build-all.sh
+++ build-all.sh
@@ -6,5 +6,5 @@ fi
 # for compat with podman on m1
 # forces p2bin to not be on mounted volume to workaround
 # https://github.com/containers/podman/issues/14142
-linux_build/build.sh --p2bin /tmp/p2bin ps4.asm out/ps4-grand-cross.bin
+linux_build/build.sh --p2bin /usr/bin/ps4p2bin ps4.asm out/ps4-grand-cross.bin
 #linux_build/build.sh ps4.asm out/ps4-grand-cross.bin
