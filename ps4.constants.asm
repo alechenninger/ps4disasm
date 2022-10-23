d3fb8ab2e5ab5178eeb334369d4d7230d06191fc
Alec Henninger <alechenninger@gmail.com>
2022-10-23 13:48:27 -0400

    Add new movement speed offsets; fix found alys tail call

--- ps4.constants.asm
+++ ps4.constants.asm
@@ -2211,7 +2211,7 @@ Found_Item_Type = ramaddr($FFFFECC4)	; 0 = content is an item; 1 = content is mo
 
 Map_Palettes_Addr = ramaddr($FFFFECD2)
 
-FieldObj_Step_Offset = ramaddr($FFFFECE0)
+FieldObj_Step_Offset = ramaddr($FFFFECE0)	; 0 is slower, 1 is normal, 2 is faster. grand cross added 3 for even slower.
 
 Tile_Coll_Run_Event_Flag = ramaddr($FFFFECE1)	; only bit 0 seems to be used; if set, it disables tile collision and event checks; this can only be changed in the Debug Window
 Field_Poison_Flag = ramaddr($FFFFECE6)	; if 0, poison won't hurt you while walking, otherwise set to 1
