9f24dfe3e30d0ebc28abfc6da8446f00c408372a
Alec Henninger <alechenninger@gmail.com>
2023-02-20 11:34:16 -0500

    Add container build script

---
+++ container-build.sh
@@ -0,0 +1,3 @@
+osascript -e 'display notification "Build starting... ('$1' files changed)" with title "ps4disasm"'
+podman run --platform linux/arm64 --mount type=bind,source=$(pwd),destination=/ps4disasm/ --workdir /ps4disasm/ --rm -ti asl /bin/bash -c /ps4disasm/build-all-grand-cross.sh
+osascript -e 'display notification "Build complete ✅" with title "ps4disasm"'
