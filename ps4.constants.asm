1983bf300dac49753fcdf8becbedad2d0c0cfec5
Alec Henninger <alechenninger@gmail.com>
2023-02-25 20:52:09 -0500

    Add extended event flags

--- ps4.constants.asm
+++ ps4.constants.asm
@@ -2329,8 +2329,9 @@ Save_Slot = ramaddr($FFFFF012)	; Starts from 0
 SRAM_Bytes_Sum = ramaddr($FFFFF014)
 
 Event_Flags = ramaddr($FFFFF100)
-Chest_Flags = ramaddr($FFFFF120)
-Temp_Event_Flags = ramaddr($FFFFF140)
+Extended_Event_Flags = ramaddr($FFFFF120)
+Chest_Flags = ramaddr($FFFFF140)
+Temp_Event_Flags = ramaddr($FFFFF156)
 Town_Flags = ramaddr($FFFFF160)
 
 World_Index = ramaddr($FFFFF400)	; 0 = Motavia; 1 = Dezolis; 2 = Rykros; 3 = Zelan; 4 = Kuran; 5 = Air Castle
