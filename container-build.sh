348a4c70a476e7e55b0fc21295564988aa033db2
Alec Henninger <alechenninger@gmail.com>
2023-03-05 15:29:52 -0500

    Rewrite dialog terminate arrow
    Embedding it into RunText broke shops and probably other things.
    
    This commit externalizes it so each RunText invocation can handle it
    accordingly.

--- container-build.sh
+++ container-build.sh
@@ -1,3 +1,4 @@
 osascript -e 'display notification "Build starting... ('$1' files changed)" with title "ps4disasm"'
 podman run --platform linux/arm64 --mount type=bind,source=$(pwd),destination=/ps4disasm/ --workdir /ps4disasm/ --rm -ti asl /bin/bash -c /ps4disasm/build-all-grand-cross.sh
+date
 osascript -e 'display notification "Build complete ✅" with title "ps4disasm"'
