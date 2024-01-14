82be85e136604b8cb56899e601d9de4b8d6a2f04
Alec Henninger <alechenninger@gmail.com>
2024-01-14 16:19:27 -0500

    Say "build complete" :D

--- container-build.sh
+++ container-build.sh
@@ -2,3 +2,4 @@ osascript -e 'display notification "Build starting... ('$1' files changed)" with
 podman run --platform linux/arm64 --mount type=bind,source=$(pwd),destination=/ps4disasm/ --workdir /ps4disasm/ --rm -ti asl /bin/bash -c /ps4disasm/build-all-grand-cross.sh
 date
 osascript -e 'display notification "Build complete ✅" with title "ps4disasm"'
+osascript -e 'say "build complete"'
